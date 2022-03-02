93386ad2022e9718a5562ccae53001a59b70e6c5
WISERTECH\yunus.karakuzu <yunus.karakuzu@wisertech.ai>
2022-03-02 14:38:35 +0300

    search,categoryItem,title, widecategoryıtem components has been added. react-indiana-drag-scroll packets setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import Navigation from "./NavBar/Navigation";
 import Auth from "./NavBar/Auth";
 import { useMatch } from "react-router-dom";
+import Search from 'components/NavBar/Search';
 function Navbar() {
   const searchRoute = useMatch("/search");
   return (
     <nav className="h-{3.75rem} flex mt-4 items-center justify-between px-8 ">
       <Navigation />
 
-      {searchRoute && <div>Search</div>}
+      {searchRoute && <Search/>}
 
       <Auth />
     </nav>
